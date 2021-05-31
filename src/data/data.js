7ef77d1e79d359930a5fc9495de41f0ef62e8ad8
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-05-31 14:39:35 +0300

    Update

--- src/data/data.js
+++ src/data/data.js
@@ -2,35 +2,35 @@ const filterData = [
 	{
 		id: '1',
 		title: 'Currencies',
-		categoryId: 'currency',
-		categoryNames: [
-			{ eur: 'Euro' },
-			{ usd: 'US Dollar' },
-			{ gbp: 'British Pound' },
 
-			{ inr: 'Rupee' },
-			{ cny: 'Yuan' },
+		categoryNames: [
+			'Euro',
+			'Dollar',
+			'Pound',
+			'Dinar',
+			'Franc',
+			'Rupee',
+			'Yuan',
 		],
 	},
 	{
 		id: '2',
 		title: 'Region',
-		categoryId: 'region',
 		categoryNames: ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'],
 	},
 	{
 		id: '3',
 		title: 'Languages',
-		categoryId: 'lang',
+
 		categoryNames: [
-			{ ru: 'Russian' },
-			{ pt: 'Portuguese ' },
-			{ ar: 'Arabic ' },
-			{ hi: 'Hindi ' },
-			{ en: 'English' },
-			{ es: 'Spanish ' },
-			{ zh: 'Chinese ' },
-			{ fr: 'French' },
+			'Russian',
+			'Portuguese ',
+			'Arabic ',
+			'Hindi ',
+			'English',
+			'Spanish ',
+			'Chinese ',
+			'French',
 		],
 	},
 ];
